bc50d469117acfd018df01cfb1d7115a20ea1b38
dextersquad4 <25liande@dextersouthfield.org>
2025-06-02 14:59:30 -0400

    finish

--- colab.py
+++ colab.py
@@ -29,7 +29,7 @@ if __name__ == "__main__":
     movie_embeddings = pd.DataFrame(data=ratings["movie"].unique(), columns=["movie"])
     movie_embeddings["embeddings"] = [1/20 * np.random.random_sample(20) - 0.025 for _ in range(len(movie_embeddings))]
 
-    num_epochs = 0
+    num_epochs = 10
     learning_rate = 0.01
     regularization_term = 0.02
 
@@ -41,7 +41,7 @@ if __name__ == "__main__":
             movie_embedding = movie_embeddings.loc[movie_embeddings["movie"] == item[2],"embeddings"].item()
             user_embedding = user_embeddings.loc[user_embeddings["user"] == item[1],"embeddings"].item()
             #dot product plus the movie and user bias constant is the prediction 
-            prediction = sigmoid((movie_embedding[:19] @ user_embedding[:19] + movie_embedding[19:] + user_embedding[19:])[0])
+            prediction = sigmoid((movie_embedding[:19] @ user_embedding[:19] + movie_embedding[19] + user_embedding[19]))
 
             loss = (item[3] - prediction)
             #to calculate RMSE
@@ -55,7 +55,7 @@ if __name__ == "__main__":
                     movie_embedding[i]-= learning_rate * (-2*loss + 2 * regularization_term * movie_embedding[i])
 
             user_embedding = user_embedding
-            for i, embed in enumerate(movie_embedding):
+            for i, embed in enumerate(user_embedding):
                 if (i != 19):
                     user_embedding[i]-= learning_rate * (-2*loss*movie_embedding[i] + 2 * regularization_term * user_embedding[i])
                 else:
@@ -79,14 +79,38 @@ if __name__ == "__main__":
 
 
     user_dt = ratings[ratings["user"] == 7].sort_values(by='rating', ascending=False)
+    liked_items = []
     print("They really liked: ")
     for item in user_dt.itertuples():
         if (item[3] == 5):
-            print(movies.loc[movies["index"] == item[2], "title"].item())
-        else: 
-            break
-        
-    user_embedding = user_embeddings[user_embeddings["user"] == 7]["embeddings"].item()
+            liked_item = movies.loc[movies["index"] == item[2], "title"].item()
+            print(liked_item)
+            liked_items.append(liked_item)
+    movies = movies[~movies["title"].isin(liked_items)]
+    user_embedding = user_embeddings[user_embeddings["user"] == 7]["embeddings"].item() 
+    
+    top_5_movies = []
+    for item in movies.itertuples():
+        movieIndex = item[1]
+        movieTitle = movies.loc[movies["index"] == movieIndex, "title"].item()
+        movieEmebedding = movie_embeddings.loc[movie_embeddings["movie"] == movieIndex, "embeddings"].item()
+        prediction = movieEmebedding[:19] @ user_embedding[:19] + movieEmebedding[19] + user_embedding[19]
+        if len(top_5_movies) < 5:
+            top_5_movies.append({"prediction": prediction, "title": movieTitle})
+            top_5_movies.sort(key=lambda x: x["prediction"], reverse=True)
+        else:
+            for i in range(4, -1, -1):
+                if prediction > top_5_movies[i]["prediction"] and i == 4:
+                    top_5_movies[i] = {"prediction": prediction, "title": movieTitle}
+                elif prediction > top_5_movies[i]["prediction"]:
+                    top_5_movies[i+1] = top_5_movies[i]
+                    top_5_movies[i] = {"prediction": prediction, "title": movieTitle}
+    print("These are the top 5 movies they would like: ")
+    for i, movie in enumerate(top_5_movies):
+        print(f"{i+1}) {movie["title"]}")
+
+
+
 
 
     
